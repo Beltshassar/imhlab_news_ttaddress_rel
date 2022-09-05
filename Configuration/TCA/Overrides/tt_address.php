e476e0ee164cee353e0784b65b212e580b67da13
Daniel Alexander Damm <dad@imh.dk>
2022-09-05 14:00:24 +0200

    Debug TCATypes #2

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -31,5 +31,7 @@ $tmp_news_rel_columns = [
 
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'tt_address',
-    '--div--;LLL:EXT:news_ttaddress_rel/Resources/Private/Language/locallang_db.xlf:tt_address.news_rel_tab,news'
+    '--div--;News:tt_address.news_rel_tab,news'
 );
+
+// '--div--;LLL:EXT:news_ttaddress_rel/Resources/Private/Language/locallang_db.xlf:tt_address.news_rel_tab,news'
