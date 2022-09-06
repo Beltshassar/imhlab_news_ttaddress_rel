38eef5511b12b6ad58808f9df1e705dc80971df7
Daniel Alexander Damm <dad@imh.dk>
2022-09-06 14:53:15 +0200

    swap 'MM_opposite_field' in TCA

--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -9,16 +9,18 @@ $tmp_ttaddress_rel_columns = [
         'label' => 'LLL:EXT:news/Resources/Private/Language/locallang_csh_news.xlf:author.description',
         'config' => [
             'type' => 'select',
+            'multiple' => 1,
             'renderType' => 'selectMultipleSideBySide',
             'foreign_table' => 'tt_address',
             // 'foreign_table_where' => ' AND ( ###PAGE_TSCONFIG_STR### = \'FALSE\' OR tt_address.pid = ###PAGE_TSCONFIG_STR### ) AND tt_address.deleted = 0 AND tt_address.hidden = 0 AND tt_address.sys_language_uid = ###REC_FIELD_sys_language_uid### ORDER BY tt_address.first_name ASC, tt_address.last_name ASC',
             'MM' => 'tx_imhlabnewsttaddressrel_news_ttaddress_mm',
+            'MM_opposite_field' => 'news',
             'size' => 10,
             'autoSizeMax' => 30,
             'maxitems' => 99,
-            'behaviour' => [
-                'allowLanguageSynchronization' => true
-            ],
+            // 'behaviour' => [
+            //     'allowLanguageSynchronization' => true
+            // ],
         ],
     ],
 
