8e59bf40b99f9d5cca4b1db419a25b91e0be68db
Daniel Alexander Damm <dad@imh.dk>
2022-09-06 11:06:51 +0200

    Test recordType on persistence mapping

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -4,10 +4,10 @@ declare(strict_types = 1);
 return [
     \IMHLab\NewsTtaddressRel\Domain\Model\News::class => [
         'tableName' => 'tx_news_domain_model_news',
-        'recordType' => 0,
+        'recordType' => '\IMHLab\NewsTtaddressRel\Domain\Model\News',
     ],
     \IMHLab\NewsTtaddressRel\Domain\Model\Address::class => [
         'tableName' => 'tt_address',
-        'recordType' => 0,
+        'recordType' => '\IMHLab\NewsTtaddressRel\Domain\Model\Address',
     ],
 ];
