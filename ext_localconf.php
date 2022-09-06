3ae67e672838c0221b70021180fcb18e5e99f601
Daniel Alexander Damm <dad@imh.dk>
2022-09-06 15:17:38 +0200

    set TYPO3_CONF_VARS class extender

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,15 +10,19 @@ defined('TYPO3_MODE') or die();
 //          */
 //         $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'news-ttaddress-rel';
 // //        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']
+    $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['news']['extender'][
+        \GeorgRinger\News\Domain\Model\News::class
+        ]['news-ttaddress-rel'] = 'EXT:news-ttaddress-rel/Classes/Domain/Model/News.php';
 
-//         /**
-//          * Extend ext:tt-address
-//          */
-//         // $GLOBALS['TYPO3_CONF_VARS']['EXT']['tt_address']['classes']['Domain/Model/Address'][] = 'news-ttaddress-rel';
-//         $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][
-//             \FriendsOfTYPO3\TtAddress\Domain\Model\Address::class
-//              ]['news-ttaddress-rel'] = 'EXT:news-ttaddress-rel/Classes/Domain/Model/Address.php';
-//              //][news-ttaddress-rel] = 'EXT:news-ttaddress-rel/Classes/Extending/Domain/Model/Address.php';
+
+        /**
+         * Extend ext:tt-address
+         */
+        // $GLOBALS['TYPO3_CONF_VARS']['EXT']['tt_address']['classes']['Domain/Model/Address'][] = 'news-ttaddress-rel';
+        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][
+            \FriendsOfTYPO3\TtAddress\Domain\Model\Address::class
+             ]['news-ttaddress-rel'] = 'EXT:news-ttaddress-rel/Classes/Domain/Model/Address.php';
+             //][news-ttaddress-rel] = 'EXT:news-ttaddress-rel/Classes/Extending/Domain/Model/Address.php';
 
 
         // call_user_func(
