03dcec76e43847ee9659164b28d6598c0c59919d
Martin Kristensen <mkr@imh.dk>
2022-10-31 14:25:40 +0100

    change path to EXT

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,7 +12,7 @@ defined('TYPO3_MODE') or die();
 
         // $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['news']['extender'][
         //     \GeorgRinger\News\Domain\Model\News::class
-        //     ]['news_ttaddress_rel'] = 'EXT:news_ttaddress_rel/Classes/Domain/Model/News.php';
+        //     ]['news_ttaddress_rel'] = 'EXT:news-ttaddress-rel/Classes/Domain/Model/News.php';
 
 
         /**
@@ -22,14 +22,14 @@ defined('TYPO3_MODE') or die();
 
         // $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][
         //     \FriendsOfTYPO3\TtAddress\Domain\Model\Address::class
-        //      ]['news_ttaddress_rel'] = 'EXT:news_ttaddress_rel/Classes/Domain/Model/Address.php';
+        //      ]['news_ttaddress_rel'] = 'EXT:news-ttaddress-rel/Classes/Domain/Model/Address.php';
 
         /**
          * Use of ext:extender
          */
         $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['news_ttaddress_rel'] = \Imhlab\NewsTtaddressRel\Extending\Domain\Model\Address::class;
         
-        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['news_ttaddress_rel'] = 'EXT:news_ttaddress_rel/Classes/Extending/Domain/Model/Address.php';
+        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['news_ttaddress_rel'] = 'EXT:news-ttaddress-rel/Classes/Extending/Domain/Model/Address.php';
         
         $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['news_ttaddress_rel'] = 'Domain/Model/Address';
 
@@ -39,6 +39,6 @@ defined('TYPO3_MODE') or die();
          * Add page TsConfig
          */
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:news_ttaddress_rel/Configuration/TsConfig/Page/TCEFORM.tsconfig">'
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:news-ttaddress-rel/Configuration/TsConfig/Page/TCEFORM.tsconfig">'
         );
 })();
