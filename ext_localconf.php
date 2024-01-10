7abd024b28e2ece5c590275f2e29448602cc489a
Daniel Alexander Damm <danieldamm@gmail.com>
2024-01-10 15:23:29 +0100

    Comment on Globals in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,7 +8,7 @@ defined('TYPO3_MODE') or die();
         /**
          * Extend ext:news
          */
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'news_ttaddress_rel';
+        $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'news_ttaddress_rel'; // gør muligvis ingenting
 
         // $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['news']['extender'][
         //     \GeorgRinger\News\Domain\Model\News::class
@@ -18,7 +18,7 @@ defined('TYPO3_MODE') or die();
         /**
          * Extend ext:tt-address
          */
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['tt_address']['classes']['Domain/Model/Address'][] = 'news_ttaddress_rel';
+        $GLOBALS['TYPO3_CONF_VARS']['EXT']['tt_address']['classes']['Domain/Model/Address'][] = 'news_ttaddress_rel'; // gør muligvis ingenting
 
         // $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][
         //     \FriendsOfTYPO3\TtAddress\Domain\Model\Address::class
@@ -28,12 +28,12 @@ defined('TYPO3_MODE') or die();
          * Use of ext:extender
          */
         // $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['news_ttaddress_rel'] = \Imhlab\NewsTtaddressRel\Extending\Domain\Model\Address::class;
-        
+
         // $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['news_ttaddress_rel'] = 'EXT:news_ttaddress_rel/Classes/Extending/Domain/Model/Address.php';
-        
+
         // $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['news_ttaddress_rel'] = 'Domain/Model/Address';
 
-        
+
 
         /**
          * Add page TsConfig
